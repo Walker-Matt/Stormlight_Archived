c0f334f9d349b815cdbdf67154903f1d4fedcbe1
Walker_Matt <walker_matt@hotmail.ca>
2021-01-28 18:15:18 -0500

    Playing around with interaction.

--- App.js
+++ App.js
@@ -6,26 +6,33 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
   Text,
   Image,
   ImageBackground,
+  Button,
 } from 'react-native';
 
-import background from './android/app/src/main/assets/images/Background.jpg'
-const image = Image.resolveAssetSource(background).uri;
+const App = () => {
+  const [count, setCount] = useState(0);
 
-const App = () => (
-  <View style={styles.container}>
-    <ImageBackground source={{uri: image}} style={styles.image}>
-      <Text style={styles.text}>Stormlight Archived</Text>
-    </ImageBackground>
-  </View>
-);
-
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require('./assets/images/background.jpg')}
+        style={styles.image}>
+        <Text style={styles.text}>Stormlight Archived</Text>
+        <Button
+          onPress={() => setCount(count + 1)}
+          title="Click me!"
+        />
+        <Text> {count} </Text>
+      </ImageBackground>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
