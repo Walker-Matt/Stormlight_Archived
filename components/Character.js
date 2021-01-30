ea3e793d23b7e2454ee55b4b142694e0f15ab243
Walker_Matt <walker_matt@hotmail.ca>
2021-01-30 13:09:56 -0500

    Character component.

---
+++ components/Character.js
@@ -0,0 +1,13 @@
+/**
+ * Character component
+ */
+import React from 'react';
+
+class Character extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+}
