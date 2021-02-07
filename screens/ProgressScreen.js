ffd409f178e829fcf853e220737353cd14e1c101
Walker_Matt <walker_matt@hotmail.ca>
2021-02-07 17:28:51 -0500

    Chapter error checking.

--- screens/ProgressScreen.js
+++ screens/ProgressScreen.js
@@ -18,11 +18,6 @@ import {Picker} from '@react-native-picker/picker';
 
 import GLOBAL from './../Global'
 
-//The Way of Kings: 75 chapters
-//Words of Radiance: 89 chapters
-//Oathbringer: 122 chapters
-//Rhythm of War: 117 chapters
-
 export default function ProgressScreen({ navigation }) {
   const [bookValue, setBookValue] = React.useState('');
   const [partValue, setPartValue] = React.useState('');
@@ -31,41 +26,37 @@ export default function ProgressScreen({ navigation }) {
   return (
     <View style={styles.column}>
       <Separator/>
-      <View style={styles.row}>
+      <View style={styles.textRow}>
         <Text style={styles.text}>
           Book:
         </Text>
       </View>
       <Separator/>
       <BookSelect
-      bookValue={bookValue}
-      setBookValue={setBookValue}/>
+        bookValue={bookValue}
+        setBookValue={setBookValue}/>
       <Separator/>
-      <View style={styles.row}>
+      <View style={styles.textRow}>
         <Text style={styles.text}>
           Part:
         </Text>
       </View>
       <Separator/>
       <PartSelect
-      partValue={partValue}
-      setPartValue={setPartValue}/>
+        partValue={partValue}
+        setPartValue={setPartValue}/>
       <Separator/>
-      <View style={styles.row}>
+      <View style={styles.textRow}>
         <Text style={styles.text}>
           Chapter:
         </Text>
       </View>
-      <View style={styles.row}>
-        <TextInput
-        style={styles.input}
-        onChangeText={text => setChapterValue(text)}
-        value={chapterValue}
-        keyboardType='number-pad'
-        />
-      </View>
       <Separator/>
-      <View style={styles.row}>
+      <ChapterSelect
+        chapterValue={chapterValue}
+        setChapterValue={setChapterValue}/>
+      <Separator/>
+      <View style={styles.contentRow}>
         <TouchableOpacity
           style={styles.save}
           onPress={() =>
@@ -128,7 +119,7 @@ function BookSelect(props) {
 
 function PartSelect(props) {
   return (
-    <View style={styles.row}>
+    <View style={styles.contentRow}>
       <Separator/>
       <TouchableOpacity
         style={(props.partValue == 1) ?
@@ -169,18 +160,106 @@ function PartSelect(props) {
   )
 }
 
+function ChapterSelect(props) {
+  return (
+    <View style={styles.contentRow}>
+      <TextInput
+      style={styles.input}
+      onChangeText={text => props.setChapterValue(text)}
+      value={props.chapterValue}
+      keyboardType='number-pad'
+      />
+    </View>
+  )
+}
+
+//The Way of Kings: 75 chapters
+//Words of Radiance: 89 chapters
+//Oathbringer: 122 chapters
+//Rhythm of War: 117 chapters
+
 function trySetProgress(navigation, bookValue, partValue, chapterValue) {
   if(!bookValue || !partValue || !chapterValue) {
     alert("Please select the book, part, and chapter you have read to.")
   } else {
-    GLOBAL.functions.SetProgress(
-      bookValue + ',' +
-      partValue + ',' +
-      chapterValue);
-      navigation.reset({
-        index: 0,
-        routes: [{ name: 'Home'}],
-      })
+    if(chapterValue % 1 != 0) {
+      alert("Please enter a valid chapter number")
+    } else {
+      //Book 1
+      if(bookValue == 1 && partValue == 1 &&
+        (chapterValue < 1 || chapterValue > 11)) {
+        alert("Please select a chapter from 1 to 11.")
+      } else if(bookValue == 1 && partValue == 2 &&
+        (chapterValue < 12 || chapterValue > 28)) {
+        alert("Please select a chapter from 12 to 28.")
+      } else if(bookValue == 1 && partValue == 3 &&
+        (chapterValue < 29 || chapterValue > 51)) {
+        alert("Please select a chapter from 29 to 51.")
+      } else if(bookValue == 1 && partValue == 4 &&
+        (chapterValue < 52 || chapterValue > 69)) {
+        alert("Please select a chapter from 52 to 69.")
+      } else if(bookValue == 1 && partValue == 5 &&
+        (chapterValue < 70 || chapterValue > 75)) {
+        alert("Please select a chapter from 70 to 75.")
+      //Book 2
+      } else if(bookValue == 2 && partValue == 1 &&
+        (chapterValue < 1 || chapterValue > 12)) {
+        alert("Please select a chapter from 1 to 12.")
+      } else if(bookValue == 2 && partValue == 2 &&
+        (chapterValue < 13 || chapterValue > 34)) {
+        alert("Please select a chapter from 13 to 34.")
+      } else if(bookValue == 2 && partValue == 3 &&
+        (chapterValue < 35 || chapterValue > 58)) {
+        alert("Please select a chapter from 35 to 58.")
+      } else if(bookValue == 2 && partValue == 4 &&
+        (chapterValue < 59 || chapterValue > 75)) {
+        alert("Please select a chapter from 59 to 75.")
+      } else if(bookValue == 2 && partValue == 5 &&
+        (chapterValue < 76 || chapterValue > 89)) {
+        alert("Please select a chapter from 76 to 89.")
+      //Book 3
+      } else if(bookValue == 3 && partValue == 1 &&
+        (chapterValue < 1 || chapterValue > 32)) {
+        alert("Please select a chapter from 1 to 32.")
+      } else if(bookValue == 3 && partValue == 2 &&
+        (chapterValue < 33 || chapterValue > 57)) {
+        alert("Please select a chapter from 33 to 57.")
+      } else if(bookValue == 3 && partValue == 3 &&
+        (chapterValue < 58 || chapterValue > 87)) {
+        alert("Please select a chapter from 58 to 87.")
+      } else if(bookValue == 3 && partValue == 4 &&
+        (chapterValue < 88 || chapterValue > 113)) {
+        alert("Please select a chapter from 88 to 113.")
+      } else if(bookValue == 3 && partValue == 5 &&
+        (chapterValue < 114 || chapterValue > 122)) {
+        alert("Please select a chapter from 114 to 122.")
+      //Book 4
+      } else if(bookValue == 4 && partValue == 1 &&
+        (chapterValue < 1 || chapterValue > 19)) {
+        alert("Please select a chapter from 1 to 19.")
+      } else if(bookValue == 4 && partValue == 2 &&
+        (chapterValue < 20 || chapterValue > 43)) {
+        alert("Please select a chapter from 20 to 43.")
+      } else if(bookValue == 4 && partValue == 3 &&
+        (chapterValue < 44 || chapterValue > 72)) {
+        alert("Please select a chapter from 44 to 72.")
+      } else if(bookValue == 4 && partValue == 4 &&
+        (chapterValue < 73 || chapterValue > 97)) {
+        alert("Please select a chapter from 73 to 97.")
+      } else if(bookValue == 4 && partValue == 5 &&
+        (chapterValue < 98 || chapterValue > 117)) {
+        alert("Please select a chapter from 98 to 117.")
+      } else {
+        GLOBAL.functions.SetProgress(
+          bookValue + ',' +
+          partValue + ',' +
+          chapterValue);
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'Home'}],
+        })
+      }
+    }
   }
 }
 
@@ -194,9 +273,14 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center"
   },
-  row: {
+  contentRow: {
     flex: 2,
     flexDirection: "row",
+  },
+  textRow: {
+    flex: 2,
+    flexDirection: "row",
+    backgroundColor: "white"
   },
   bookRow: {
     flex: 4,
@@ -251,7 +335,7 @@ const styles = StyleSheet.create({
   text: {
     flex: 1,
     color: "black",
-    fontSize: 30,
+    fontSize: 40,
     textAlign: "center"
   },
   save: {
